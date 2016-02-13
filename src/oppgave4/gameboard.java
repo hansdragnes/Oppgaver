64998373d241764b7e91e7106cffef18bd034b66
Hans Dragnes <hdragnes@gmail.com>
2016-02-13 10:36:33 +0100

    Updated console output to be more readable. Adjusting the gameboard array manually enables testing.

--- src/oppgave4/gameboard.java
+++ src/oppgave4/gameboard.java
@@ -1,11 +1,13 @@
 package oppgave4;
 
+import java.util.Arrays;
+
 public class gameboard {
 
 	int[][] table = 
-			new int [][]	{{1,0,0},
-		 					{1,0,1},
-		 					{1,0,0}
+			new int [][]	{{0,0,0},
+		 					{1,1,1},
+		 					{1,1,0}
 							};
 
 	public int[][] getTable() {
@@ -15,5 +17,16 @@ public class gameboard {
 	public void setTable(int[][] table) {
 		this.table = table;
 	}
+
+	@Override
+	public String toString() {
+		String o="";
+		for (int i=0;i<table.length;i++)
+			for (int j=0;j<table[i].length;j++)
+				o+=table[i][j];
+		return o;
+	}
+	
+	
 	
 }
